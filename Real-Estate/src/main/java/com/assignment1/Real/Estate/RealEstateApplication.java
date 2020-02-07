1dcecaaa8decbe2757251ec3fdceb283f2e4985a
Aryanna Anderson <tuh05038@temple.edu>
2020-02-07 01:14:23 -0500

    Created separate files for each test and each statistic function

--- Real-Estate/src/main/java/com/assignment1/Real/Estate/RealEstateApplication.java
+++ Real-Estate/src/main/java/com/assignment1/Real/Estate/RealEstateApplication.java
@@ -1,25 +1,18 @@
 package com.assignment1.Real.Estate;
 
-
-
-
 import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.assignment1.Real.Estate.field;
+import com.assignment1.Real.Estate.getStat;
 
 @SpringBootApplication
 public class RealEstateApplication {
 
-	 
-	
-	
 	public static void main(String[] args) throws IOException {
-		field.parseData();
+		getStat.parseData();
 		SpringApplication.run(RealEstateApplication.class, args);
-		
-		
+
 	}
-	
+
 }
--- Real-Estate/src/main/java/com/assignment1/Real/Estate/getStat.java
+++ Real-Estate/src/main/java/com/assignment1/Real/Estate/getStat.java
@@ -10,14 +10,13 @@ import java.util.stream.StreamSupport;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.apache.commons.math3.util.Precision;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class field {
+public class getStat {
 
 	public static List<List<String>> table = new ArrayList<List<String>>();
 
@@ -48,88 +47,30 @@ public class field {
 		if (field.compareTo("price") == 0) {
 			index = 9;
 		}
-		if (field.compareTo("squareFootage") == 0 || field.compareTo("sq_ft") == 0) {
+		if (field.compareTo("squarefootage") == 0 || field.compareTo("sq_ft") == 0) {
 			index = 6;
 		}
 
 		if (statistic.compareTo("min") == 0 || statistic.compareTo("minimum") == 0) {
-			stat = getMin(index);
+			stat = minStat.getMin(index);
 		}
 
 		if (statistic.compareTo("max") == 0 || statistic.compareTo("maximum") == 0) {
-			stat = getMax(index);
+			stat = maxStat.getMax(index);
 		}
 
 		if (statistic.compareTo("avg") == 0 || statistic.compareTo("average") == 0) {
-			stat = getAvg(index);
+			stat = avgStat.getAvg(index);
 		}
 
 		if (statistic.compareTo("sum") == 0) {
-			stat = getSum(index);
+			stat = sumStat.getSum(index);
 		}
 
 		if (statistic.compareTo("range") == 0) {
-			stat = getRange(index);
+			stat = rangeStat.getRange(index);
 		}
 		return stat;
-	}
-
-	public double getMin(int index) {
-		double min = Integer.parseInt(table.get(0).get(index));
-		int i = 1;
-		double next;
-		while (i < table.size()) {
-			next = Integer.parseInt(table.get(i).get(index));
-			if (min >= next) {
-				min = next;
-			}
-			i++;
-		}
-		return min;
-	}
-
-	public double getMax(int index) {
-		double max = Integer.parseInt(table.get(0).get(index));
-		int i = 1;
-		double next;
-		while (i < table.size()) {
-			next = Integer.parseInt(table.get(i).get(index));
-			if (max <= next) {
-				max = next;
-			}
-			i++;
-		}
-		return max;
-	}
-
-	public double getAvg(int index) {
-		double avg;
-		int i = 0;
-		double totalSum = 0;
-		while (i < table.size()) {
-			int stat = Integer.parseInt(table.get(i).get(index));
-			totalSum += stat;
-			i++;
-		}
-		avg = totalSum / i;
-		return Precision.round(avg, 2);
-	}
-
-	public double getSum(int index) {
-		int i = 0;
-		double sum = 0;
-		double stat;
-		while (i < table.size()) {
-			stat = Integer.parseInt(table.get(i).get(index));
-			sum += stat;
-			i++;
-		}
-		return sum;
-	}
-
-	public double getRange(int index) {
-		double range = getMax(index) - getMin(index);
-		return range;
 	}
 
 }
